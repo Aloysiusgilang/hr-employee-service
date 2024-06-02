9b194296e9a3bbf5cf5b96cdc9e0394f36898278
Aloysius Gilang <gilangpramdya84@gmail.com>
2024-06-02 09:46:49 +0700

    chore:cors

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./router";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const port = process.env.PORT || "8002";
 app.use(routes);
